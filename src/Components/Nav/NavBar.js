3a8e1758367e4b6eb87e077ddf34f9cb1e15c735
DeMarcus Kennedy <demarcus@demarcuss-air.lan>
2020-05-13 16:20:53 -0500

    fixed header

--- src/Components/Nav/NavBar.js
+++ src/Components/Nav/NavBar.js
@@ -4,7 +4,7 @@ import { Navbar, Nav } from "react-bootstrap";
 
 export default function NavBar() {
   return (
-    <Navbar bg="dark" variant="dark">
+    <Navbar bg="dark" variant="dark" fixed="top">
       <Navbar.Brand href="#home">Kennedy Fitness</Navbar.Brand>
       <Nav className="ml-auto">
         <Nav.Link href="#about">About</Nav.Link>
