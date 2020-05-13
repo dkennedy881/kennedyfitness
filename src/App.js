e4b14d51f01ced81ee2b0dc6f096d67a2e8adc3c
DeMarcus Kennedy <demarcus@demarcuss-air.lan>
2020-05-13 12:37:19 -0500

    added basic black header with page targets

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+//comps
+import NavBar from "./Components/Nav/NavBar";
 
 function App() {
   return (
     <div className="App">
-
+      <NavBar></NavBar>
     </div>
   );
 }
