44d890e15cb08652091e0f760e382f28539c2aa1
DeMarcus Kennedy <demarcus@DeMarcuss-MBP.lan1>
2020-09-06 16:53:59 -0500

    express app
    Signed-off-by: DeMarcus Kennedy <demarcus@DeMarcuss-MBP.lan1>

---
+++ client/build/precache-manifest.1b6eb06d3fa41f57409eb6660c88e56d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba355bdd54db44ee1db9280e7dcf13b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "de9d48a0c9b31cfe5139",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "32174a003581f06a4c64",
+    "url": "/static/css/main.489e99a3.chunk.css"
+  },
+  {
+    "revision": "de9d48a0c9b31cfe5139",
+    "url": "/static/js/2.12926488.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.12926488.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32174a003581f06a4c64",
+    "url": "/static/js/main.5eb5e438.chunk.js"
+  },
+  {
+    "revision": "c53c28e51162bd26cd6c",
+    "url": "/static/js/runtime-main.333cea12.js"
+  },
+  {
+    "revision": "6e3d411cf792582d58081a9fb5f70857",
+    "url": "/static/media/runninglady.6e3d411c.png"
+  }
+]);
