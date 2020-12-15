0ade9b15bf7fc951c8ad652c467bfc55a8ea7149
DeMarcus Kennedy <demarcus@demarcuss-mbp.lan>
2020-12-14 20:55:48 -0600

    forgot to build
    Signed-off-by: DeMarcus Kennedy <demarcus@demarcuss-mbp.lan>

---
+++ client/build/precache-manifest.cb2a5857f4bd2740c6198910233232f2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b259fd9fce9d0aac04f4de42c807e191",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fb36efa6ddd078d22b08",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "67440cb07ca9c4edd22e",
+    "url": "/static/css/main.b0207f4a.chunk.css"
+  },
+  {
+    "revision": "fb36efa6ddd078d22b08",
+    "url": "/static/js/2.dd0674ff.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.dd0674ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67440cb07ca9c4edd22e",
+    "url": "/static/js/main.643ccad5.chunk.js"
+  },
+  {
+    "revision": "c53c28e51162bd26cd6c",
+    "url": "/static/js/runtime-main.333cea12.js"
+  },
+  {
+    "revision": "8eaf69a9170ca24d17ded6de0c6f9b6d",
+    "url": "/static/media/keke_small.8eaf69a9.jpeg"
+  }
+]);
--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9daeb519a8fcbdda21beaf90a83b7d72.js"
+  "/precache-manifest.cb2a5857f4bd2740c6198910233232f2.js"
 );
 
 self.addEventListener('message', (event) => {
